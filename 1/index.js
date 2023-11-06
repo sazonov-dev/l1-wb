363c69ee6657dbfb5eaee9356cff562e0844ce45
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-11-06 17:48:54 +0300

    Добавлены комментарии

--- 1/index.js
+++ 1/index.js
@@ -1,11 +1,11 @@
 const isPalindrome = (string) => {
-    string = string.toLowerCase();
-    const result = string.split(' ').join('').split('').reverse().join('');
+    string = string.toLowerCase(); // Приводим строку к нижнему регистру
+    const result = string.split(' ').join('').split('').reverse().join(''); // Набор методов с помощью которого получается перевернутая строка
 
-    return string.split(' ').join('') === result;
+    return string.split(' ').join('') === result; // Проверяем является ли исходная строка палиндромом измененной строки
 }
 
-console.log(isPalindrome('Коту скоро сорок суток'))
-console.log(isPalindrome('Шалаш'))
-console.log(isPalindrome('мыло'))
-console.log(isPalindrome('привет'))
+console.log(isPalindrome('Коту скоро сорок суток')) // true
+console.log(isPalindrome('Шалаш')) // true
+console.log(isPalindrome('мыло')) // false
+console.log(isPalindrome('привет')) // false

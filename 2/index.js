334fbd4b2a39849cc4c34acad0fe4de14ca5eecd
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-11-06 17:48:01 +0300

    Добавлены комментарии

--- 2/index.js
+++ 2/index.js
@@ -1,13 +1,15 @@
 const isStrangeNumber = (number) => {
     let sum = 0;
 
-    for (let i = 1; i <= number / 2; i++) {
-        if (number % 1 === 0) {
+    for (let i = 1; i <= number / 2; i++) { // Идем циклом до половины самого числа, чтобы найти верные делители.
+        if (number % i === 0) { // Проверяем делится ли число на делитель без остатка, если да, то добавляем его к сумме
             sum += i;
         }
     }
 
-    return sum === number;
+    return sum === number; // Если сумма равна заданному числу, то это странное число
 }
 
-console.log(isStrangeNumber(6))
+console.log(isStrangeNumber(6)) // true
+console.log(isStrangeNumber(8)) // false
+console.log(isStrangeNumber(28)) // true
